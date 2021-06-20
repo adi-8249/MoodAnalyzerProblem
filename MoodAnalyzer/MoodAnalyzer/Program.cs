944e3c718691de06ca3976d7d78e287c1c3a4846
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-20 20:15:36 +0530

    [Anjaneya]Added: Welcome message for MoodAnalyzerProgram

---
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MoodAnalyzer program");
+        }
+    }
+}
